b348dec81c8324efd98a10d530b9c31b5acb82b7
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2022-01-13 10:30:40 +0100

    fpu_allocator: add the implementation for fpu_allocator::alloc and the necessary helper functions

--- include/fpu_allocator.hpp
+++ include/fpu_allocator.hpp
@@ -6,11 +6,39 @@
  * Allocates pd-local memory for the FPU.
  */
 
+#include "assert.hpp"
+#include "math.hpp"
+#include "memory.hpp"
+#include "spinlock.hpp"
+
+class Space_mem;
+
 // it should be relatively easy to turn this into a more generic allocator later
 class Fpu_allocator 
 {
+private:
+    Spinlock lock_;
+
+    // the memory for the fpu has to be 64-bytes aligned, otherwise xsave/
+    // xsaveopt and xrstor will throw a general protection exception
+    static constexpr size_t ALIGNMENT { 64 };
+
+    const size_t ELEM_SIZE;  // size of an element (fpu state + alignment)
+    const size_t ELEM_COUNT; // maximum amount of elements
+
+    size_t free_chunks_; // allocated freed chunks are bitwise encoded here
+    size_t last_chunk_;  // the first unallocated chunk
+
+    Space_mem* space_mem_;
+
 public:
-    Fpu_allocator () = default;
+    Fpu_allocator(size_t fpu_size, Space_mem* space_mem)
+        : ELEM_SIZE(align_up(fpu_size+1, ALIGNMENT))
+        , ELEM_COUNT((SPC_LOCAL_FPU_E - SPC_LOCAL_FPU) / ELEM_SIZE)
+        , free_chunks_(0)
+        , last_chunk_(0)
+        , space_mem_(space_mem)
+    { }
 
     // allocates memory from the fpu-space and returns a pointer to the
     // allocated memory
@@ -18,4 +46,30 @@ public:
 
     // marks the memory at the given address as free
     void free(void* vaddr);
+
+private:
+    // Returns the virtual address for the element with the given index
+    inline mword idx_to_virt(size_t idx)
+    {
+        assert (idx < ELEM_COUNT);
+        return SPC_LOCAL_FPU + idx * ELEM_SIZE;
+    }
+
+    // Returns the virtual address for the element with the given index. If no
+    // writeable mapping for the given index exists, this function allocates an
+    // empty page from the kernel heap and maps it.
+    mword walk(size_t idx);
+
+    // checks if a writeable mapping for the given virtual address exists. If
+    // it does, this function does nothing. Otherwise it allocates an empty
+    // page from the kernel heap and maps it so it backs vaddr.
+    void allocate_if_necessary(mword vaddr);
+
+    // This function checks if the element at the given address crosses a page
+    // boundary and returns true if it does, otherwise returns false.
+    inline bool touches_next_page(mword vaddr)
+    {
+        return (vaddr & ~PAGE_MASK) != ((vaddr + ELEM_SIZE - 1) & ~PAGE_MASK);
+    }
+
 };
---
+++ src/fpu_allocator.cpp
@@ -0,0 +1,50 @@
+#include "fpu_allocator.hpp"
+
+#include "buddy.hpp"
+#include "hpt.hpp"
+#include "lock_guard.hpp"
+#include "space_mem.hpp"
+
+void* Fpu_allocator::alloc()
+{
+    Lock_guard<Spinlock> guard(lock_);
+
+    if (free_chunks_ > 0) {
+        const long int free_idx { bit_scan_forward(free_chunks_) };
+        free_chunks_ = free_chunks_ & ~(1 << free_idx);
+        return reinterpret_cast<void*>(walk(free_idx));
+    }
+
+    const size_t free_idx { last_chunk_++ };
+    return reinterpret_cast<void*>(walk(free_idx));
+}
+
+mword Fpu_allocator::walk(size_t idx)
+{
+    const mword vaddr { idx_to_virt(idx) };
+    allocate_if_necessary(vaddr);
+
+    if (touches_next_page(vaddr)) {
+        allocate_if_necessary(idx_to_virt(idx+1));
+    }
+
+    return vaddr;
+}
+
+void Fpu_allocator::allocate_if_necessary(mword vaddr)
+{
+    const Paddr frame_0 = Buddy::ptr_to_phys(&PAGE_0);
+    Paddr paddr;
+
+    if (space_mem_->lookup(vaddr, &paddr) && (paddr & ~PAGE_MASK) != frame_0) {
+        // if a mapping for vaddr is present and the mapping does not
+        // point to the zero page, there is nothing to do
+        return;
+    }
+
+    void* empty_page_vaddr { Buddy::allocator.alloc(0, Buddy::FILL_0) };
+    Paddr empty_page_paddr { Buddy::ptr_to_phys(empty_page_vaddr)     };
+    space_mem_->replace(vaddr,
+            empty_page_paddr | Hpt::PTE_NX | Hpt::PTE_D | Hpt::PTE_A
+                             | Hpt::PTE_W | Hpt::PTE_P);
+}
